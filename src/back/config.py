794a613adce9745e2850663ad9535f87a6c1a0ad
Saar Gamzo <88244818+SaarGamzo@users.noreply.github.com>
2024-06-15 21:02:10 +0300

    Remove redundant comments

--- src/back/config.py
+++ src/back/config.py
@@ -1,9 +1,3 @@
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345678@localhost/store'   # TODO change to your root password 
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-#     # TODO - update to cloud based db
-
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:A123456@localhost/store'   # TODO change to your root password 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
